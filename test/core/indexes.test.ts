cd8e89f10d8fd0f2063e0a0ddf5bb842e353db1a
Alex Corvi <ali.a.saleem@outlook.com>
2020-04-17 17:32:33 +0300

    Update indexes.test.ts

--- test/core/indexes.test.ts
+++ test/core/indexes.test.ts
@@ -1,20 +1,8 @@
-import { assert, expect, should, use } from "chai";
+import { Index } from "@core";
+import { assert, should, use } from "chai";
 import * as asPromised from "chai-as-promised";
-import * as fs from "fs";
-import * as path from "path";
 import * as _ from "underscore";
-import { promisify } from "util";
-import {
-	Datastore,
-	Cursor,
-	Index,
-	Persistence,
-	customUtils,
-	BST,
-	storage,
-	AVLTree,
-	model,
-} from "@core";
+
 use(asPromised);
 should();
 
