5f3555b1fac6dea37f010748fd2ea02e538675c2
Alex Corvi <ali.a.saleem@outlook.com>
2020-04-17 17:34:44 +0300

    Update customUtil.test.ts

--- test/core/customUtil.test.ts
+++ test/core/customUtil.test.ts
@@ -1,19 +1,7 @@
-import { assert, expect, should, use } from "chai";
+import { customUtils } from "@core";
+import { should, use } from "chai";
 import * as asPromised from "chai-as-promised";
-import * as fs from "fs";
-import * as path from "path";
 import * as _ from "underscore";
-import { promisify } from "util";
-import {
-	Datastore,
-	Cursor,
-	Index,
-	Persistence,
-	customUtils,
-	BST,
-	storage,
-	AVLTree,
-} from "@core";
 use(asPromised);
 should();
 
