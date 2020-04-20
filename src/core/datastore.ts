143f3876e99c4efe9ddb7386fd5c5302516e667d
Alex Corvi <ali.a.saleem@outlook.com>
2020-04-20 19:34:12 +0300

    Update datastore.ts

--- src/core/datastore.ts
+++ src/core/datastore.ts
@@ -15,26 +15,10 @@ interface EnsureIndexOptions {
 
 export interface DataStoreOptions {
 	ref: string;
-	// (optional): hook you can use to transform data after it was serialized and before it is written to disk.
-	// Can be used for example to encrypt data before writing database to disk.
-	// This function takes a string as parameter (one line of an datastore data file) and outputs the transformed string, which must absolutely not contain a \n character (or data will be lost)
 	afterSerialization?(line: string): string;
-	// (optional): reverse of afterSerialization.
-	// Make sure to include both and not just one or you risk data loss.
-	// For the same reason, make sure both functions are inverses of one another.
-	// Some failsafe mechanisms are in place to prevent data loss if you misuse the serialization hooks:
-	// Datastore checks that never one is declared without the other, and checks that they are reverse of one another by testing on random strings of various lengths.
-	// In addition, if too much data is detected as corrupt,
-	// Datastore will refuse to start as it could mean you're not using the deserialization hook corresponding to the serialization hook used before (see below)
 	beforeDeserialization?(line: string): string;
-	// (optional): between 0 and 1, defaults to 10%. Datastore will refuse to start if more than this percentage of the datafile is corrupt.
-	// 0 means you don't tolerate any corruption, 1 means you don't care
 	corruptAlertThreshold?: number;
-	// (optional, defaults to false)
-	// timestamp the insertion and last update of all documents, with the fields createdAt and updatedAt. User-specified values override automatic generation, usually useful for testing.
 	timestampData?: boolean;
-	// (optional, defaults to memory only)
-	// a persistence adapter that extends the Persistence class
 	persistence_adapter?: typeof Persistence;
 }
 
