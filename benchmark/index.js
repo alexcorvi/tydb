5b210d2ed59c60e7aaf94a647fbada8e4a801148
Alex Corvi <ali.a.saleem@outlook.com>
2020-04-26 17:16:05 +0300

    benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -2,30 +2,77 @@ const Benchmark = require("benchmark");
 const lib = require("../dist/index");
 const suite = new Benchmark.Suite();
 
-const db = new lib.Database({ ref: "ref", model: lib.BaseModel });
+const memoryDB = new lib.Database({ ref: "ref", model: lib.BaseModel });
+const persistentDB = new lib.Database({
+	ref: "workspace/benchmark",
+	model: lib.BaseModel,
+	persistence_adapter: lib.FS_Persistence_Adapter,
+});
 
 let id = "";
 
 suite
-	.add("Insertion single", {
+	.add("in memory | Creating", {
 		defer: true,
 		fn: async function (deferred) {
-			const res = await db.insert([{ name: "john", age: 22 }]);
+			const res = await memoryDB.insert([{ name: "john", age: 22 }]);
 			id = res.docs[0]._id;
 			deferred.resolve();
 		},
 	})
-	.add("Finding with a non-indexed field", {
+	.add("in memory | Updating", {
 		defer: true,
 		fn: async function (deferred) {
-			await db.find({ filter: { name: "john" } });
+			await memoryDB.update({
+				filter: { _id: id },
+				update: { name: "alex" },
+			});
 			deferred.resolve();
 		},
 	})
-	.add("Finding with an indexed field", {
+	.add("in memory | Reading", {
 		defer: true,
 		fn: async function (deferred) {
-			await db.find({ filter: { _id: id } });
+			await memoryDB.find({ filter: { _id: id } });
+			deferred.resolve();
+		},
+	})
+	.add("in memory | Deleting", {
+		defer: true,
+		fn: async function (deferred) {
+			await memoryDB.delete({ filter: { _id: id } });
+			deferred.resolve();
+		},
+	})
+	.add("persistent | Creating", {
+		defer: true,
+		fn: async function (deferred) {
+			const res = await persistentDB.insert([{ name: "john", age: 22 }]);
+			id = res.docs[0]._id;
+			deferred.resolve();
+		},
+	})
+	.add("persistent | Updating", {
+		defer: true,
+		fn: async function (deferred) {
+			await persistentDB.update({
+				filter: { _id: id },
+				update: { name: "alex" },
+			});
+			deferred.resolve();
+		},
+	})
+	.add("persistent | Reading", {
+		defer: true,
+		fn: async function (deferred) {
+			await persistentDB.find({ filter: { _id: id } });
+			deferred.resolve();
+		},
+	})
+	.add("persistent | Deleting", {
+		defer: true,
+		fn: async function (deferred) {
+			await persistentDB.delete({ filter: { _id: id } });
 			deferred.resolve();
 		},
 	})
