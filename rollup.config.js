6131525dec1883d2850b986778578e54ee3e70b5
Alex Corvi <ali.a.saleem@outlook.com>
2020-04-27 17:16:57 +0300

    build bin

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json";
 import ts from "@wessberg/rollup-plugin-ts";
+import shebang from "rollup-plugin-add-shebang";
 
 export default [
 	// Browser Version
@@ -20,8 +21,9 @@ export default [
 				name: "replacer",
 				transform: function (code) {
 					// keep only IDB when in browser
-					code = code.replace(/^(.*)\/\/__IMPORT_IDB/gim, `$1`);
-					code = code.replace(/^(.*)\/\/__IMPORT_FS/gim, ``);
+					code = code.replace(/^(.*)\/\/__IMPORT_IDB/gim, "$1");
+					code = code.replace(/^(.*)\/\/__IMPORT_FS/gim, "");
+					code = code.replace(/^.*cross-fetch.*/gim, "");
 					return { code };
 				},
 			},
@@ -35,17 +37,31 @@ export default [
 			file: pkg.main,
 			format: "cjs",
 		},
+		plugins: [ts()],
+		external: [
+			"fs",
+			"path",
+			"readline",
+			"util",
+			"idb-keyval",
+			"lockfile",
+			"p-queue",
+			"cross-fetch",
+		],
+	},
+	// bin
+	{
+		input: "./src/bin.ts",
+		output: {
+			name: pkg.name,
+			file: pkg.bin.tydb,
+			format: "cjs",
+		},
 		plugins: [
 			ts(),
-			{
-				name: "replacer",
-				transform: function (code) {
-					// include both when in node
-					code = code.replace(/^(.*)\/\/__IMPORT_IDB/gim, `$1`);
-					code = code.replace(/^(.*)\/\/__IMPORT_FS/gim, `$1`);
-					return { code };
-				},
-			},
+			shebang({
+				include: pkg.bin.tydb,
+			}),
 		],
 		external: [
 			"fs",
@@ -55,6 +71,10 @@ export default [
 			"idb-keyval",
 			"lockfile",
 			"p-queue",
+			"cross-fetch",
+			"fastify",
+			"ow",
+			"fastify-cors",
 		],
 	},
 ];
