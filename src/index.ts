72696f2e1c4b925f44e859ef186f6076fbb1cd54
Alex Corvi <ali.a.saleem@outlook.com>
2020-05-01 16:55:06 +0300

    export persistence event and database configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export { Database } from "./database";
+export { Database, DatabaseConfigurations } from "./database";
 export { BaseModel } from "./types/base-schema";
+export { Persistence, PersistenceEvent } from "./core";
 
 export { IDB_Persistence_Adapter } from "./adapters/indexeddb"; //__IMPORT_IDB
 export { FS_Persistence_Adapter } from "./adapters/fs-adapter"; //__IMPORT_FS
